3685ee96d2cf17e189db9eef9ecf151412c891aa
Lisa Herzberg <lisaherzberg@Lisas-MacBook-Pro.local>
2019-03-19 19:24:26 -0700

    added gyft project pictures

--- src/screens/Projects/Gyft.js
+++ src/screens/Projects/Gyft.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import LightboxProject from "../LightboxProject";
 
+const images = [
+    'https://s3-us-west-1.amazonaws.com/portfoliolisa/gyft/GyftHomeSlider1.png',
+    'https://s3-us-west-1.amazonaws.com/portfoliolisa/gyft/gyftpage2.png',
+    'https://s3-us-west-1.amazonaws.com/portfoliolisa/gyft/GyftTech3.png'
+]
+
 class Gyft extends Component {
     constructor(props){
-        super(props)
+        super(props);
         this.state = {
             open: false,
+            hover: false
         }
     }
 
@@ -13,20 +20,37 @@ class Gyft extends Component {
         this.setState({
             open: !this.state.open
         })
-    }
+    };
 
     close = () => {
         this.setState({
             open: !this.state.open
         })
-    }
+    };
+
+    hover = () => {
+        this.setState({
+            hover: true
+        })
+    };
+
+    hoverOut = () => {
+        this.setState({
+            hover: false
+        })
+    };
 
     render() {
         return (
             <div>
-                <LightboxProject open={this.state.open} close={this.close}/>
-                <div className="project">
-                    <img src="https://s3-us-west-1.amazonaws.com/portfoliolisa/gyft/longgyft.png" width='100%' className="project" onClick={this.openLightBox}/>
+                <LightboxProject open={this.state.open} close={this.close} images={images}/>
+                <div className="project" onClick={this.openLightBox}>
+                    {!this.state.hover ? (
+                        <img src="https://s3-us-west-1.amazonaws.com/portfoliolisa/gyft/GyftThumbNail.png" width='100%' onMouseOver={this.hover} onMouseOut={this.hoverOut}/>
+                    ) : (
+                        <img src="https://s3-us-west-1.amazonaws.com/portfoliolisa/gyft/GyftThumbNailHiover.png" width='100%' onMouseOver={this.hover} onMouseOut={this.hoverOut}/>
+                    )
+                    }
                 </div>
             </div>
         );
